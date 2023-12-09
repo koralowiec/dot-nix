e03d97fbda97fdb5a044cb73c9071e7432d1a93a
koralowiec <36413794+koralowiec@users.noreply.github.com>
2023-12-09 12:44:36 +0100

    fix: update NixOS config

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -1,1 +1,242 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-7b524eb6-6f0a-43e6-9314-33d6a01302a6".device = "/dev/disk/by-uuid/7b524eb6-6f0a-43e6-9314-33d6a01302a6";
+  boot.initrd.luks.devices."luks-7b524eb6-6f0a-43e6-9314-33d6a01302a6".keyFile = "/crypto_keyfile.bin";
+
+  # The latest kernel
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Specific kernel version: 6.4
+  boot.kernelPackages = pkgs.linuxPackages_6_4;
+
+  networking.hostName = "microwave"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Disable IPv6
+  networking.enableIPv6 = false;
+  boot.kernelParams = ["ipv6.disable=1"];
+
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pl_PL.UTF-8";
+    LC_IDENTIFICATION = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MONETARY = "pl_PL.UTF-8";
+    LC_NAME = "pl_PL.UTF-8";
+    LC_NUMERIC = "pl_PL.UTF-8";
+    LC_PAPER = "pl_PL.UTF-8";
+    LC_TELEPHONE = "pl_PL.UTF-8";
+    LC_TIME = "pl_PL.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Install i3 related packages
+  services.xserver.windowManager.i3 = {
+    package = pkgs.i3-gaps;
+    enable = true;
+    extraPackages = with pkgs; [
+      feh
+      i3status
+      i3lock
+      i3lock-color
+      i3blocks
+      rofi
+    ];
+  };
+
+  # Configure video drivers for X server
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  # Configure dedicated GPU as the first Device section for X server
+  services.xserver.deviceSection = ''
+    BusID "PCI:3:0:0"
+  '';
+
+  # Configure fonts
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  # Set up i2c permissions
+  # For ddcutil
+  hardware.i2c.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "pl";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "pl2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  # Install driver for Samsung printer
+  services.printing.drivers = [ pkgs.splix ];
+
+  # Enable SANE library for scanner support
+  hardware.sane.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.arek = {
+    isNormalUser = true;
+    description = "arek";
+    extraGroups = [ 
+      "networkmanager"
+      "wheel"
+      # For adb
+      "adbusers"
+      # For running ddcutil without sudo
+      "i2c"
+      # Scanner
+      "scanner"
+      "lp" 
+      # libvirt
+      "libvirtd"
+      "qemu-libvirtd"
+    ];
+    packages = with pkgs; [
+      firefox
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+  ];
+
+  # Configure Podman
+  virtualisation.podman = {
+    enable = true;
+  };
+
+  # Configure rootless Docker
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+
+  # Configure libvirt
+  virtualisation.libvirtd = {
+    enable = true;
+  };
+
+  # Generate the manual page index caches
+  documentation.man.generateCaches = true;
+
+  # Install Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # Install Mulvad VPN
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # ?
+  # services.fwupd.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  # Flatpak
+  services.flatpak.enable = true;
+
+  # Driver for Xbox Adapter
+  hardware.xone.enable = true;
+}
