f299851a374ba55b18d1eee09044fa0ee6f6df7d
koralowiec <36413794+koralowiec@users.noreply.github.com>
2025-03-19 16:34:48 +0100

    feat(tmux): remove plugins

--- home-manager/cli/tmux.nix
+++ home-manager/cli/tmux.nix
@@ -2,40 +2,19 @@
 { 
   programs.tmux = {
     enable = true;
-    baseIndex = 1;
+    baseIndex = 0;
     escapeTime = 0;
     extraConfig = ''
       # Fixes the issue with incorrect colors in nvim
       # https://github.com/mhartington/oceanic-next/issues/51#issuecomment-1028541987
       # set -ga terminal-overrides \",*256col*:Tc\"
 
-      # Reassign the Alt+f combination to send the Alt+f keystrokes
-      # directly to the bash shell within tmux
-      # Fixes the issue after tilish installation
-      bind-key -n M-f send-keys M-f
-
-      set -g @urlview-key 'x'
       # https://github.com/alacritty/alacritty/issues/3354#issuecomment-1585708714
       set-option -ga terminal-overrides ",alacritty:Tc"
       # set -g default-terminal "alacritty"
     '';
     historyLimit = 10000;
     keyMode = "vi";
-    plugins = with pkgs; [
-      {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavour 'macchiato'
-        '';
-      }
-      {
-        plugin = tmuxPlugins.tilish;
-        extraConfig = ''
-          set -g @tilish-default 'tiled'
-        '';
-      }
-      tmuxPlugins.tmux-thumbs
-    ];
-    tmuxp.enable = true;
+    plugins = with pkgs; [];
   };
 }
